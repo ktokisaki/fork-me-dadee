040919c0dbdd2024fee1dc13876ff5abb86fc8df
ktokisaki <71679760+ktokisaki@users.noreply.github.com>
2020-09-22 09:52:10 +0800

    Create commandline.c

---
+++ commandline.c
@@ -0,0 +1,10 @@
+// FROM 0x266 COMMAND-LINE ARGUMENTS
+#include <stdio.h>
+
+int main(int arg_count, char *arg_list[]) {
+	printf("There were %d arguments provided.\n", arg_count);
+	for(int i = 0; i < arg_count; i++){
+		printf("argument #%d\t-\t%s\n", i, arg_list[i]);
+	}
+	return 0;
+}
